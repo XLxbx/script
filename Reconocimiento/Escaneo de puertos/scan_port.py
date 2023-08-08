44c05a2287d7dba6a30da41a3f8fc666406ab590
Lxbx <117536013+XLxbx@users.noreply.github.com>
2023-08-07 20:25:53 -0400

    scan_port.py

--- Reconocimiento/Escaneo de puertos/scan_port.py
+++ Reconocimiento/Escaneo de puertos/scan_port.py
@@ -1,18 +1,25 @@
 import sys
-import socket 
+import socket
 
-target = socket.gethostbyname(input("Ingrese IP ==> | "))
+def scan_ports(target):
+    try:
+        print("[+] Scanning in progress for " + target)
+        for port in range(20, 6000):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(1)
+                result = s.connect_ex((target, port))
+                if result == 0:
+                    print("[+] Port open => {} ".format(port))
+    except KeyboardInterrupt:
+        print("\n[+] Scan interrupted by the user.")
+        sys.exit(0)
+    except socket.gaierror:
+        print("[-] Invalid hostname or address.")
+        sys.exit(1)
+    except socket.error:
+        print("[-] Could not connect to the target.")
+        sys.exit(1)
 
-print("[+] Escaneo en curso......" + target)
-
-try: 
-    for port in range(1,1000):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
-            resultado = s.connect_ex((target, port))
-            if resultado == 0: 
-                print("[+] Port open => {} ".format(port))
-            s.close
-except: 
-    print("[-] Sin resultado")
-    sys.exit(0)
+if __name__ == "__main__":
+    target = input("Enter IP or hostname: ").strip()
+    scan_ports(target)
